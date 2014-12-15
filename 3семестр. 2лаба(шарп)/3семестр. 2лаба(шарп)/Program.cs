b7f7493c76dd4451ab0f594c41f49a1f50da4bfb
Lerap <justlera46@gmail.com>
2014-12-15 20:23:35 +0200

    Change 15.12.2014
    Line in console before program execution is added

--- 3семестр. 2лаба(шарп)/3семестр. 2лаба(шарп)/Program.cs
+++ 3семестр. 2лаба(шарп)/3семестр. 2лаба(шарп)/Program.cs
@@ -11,6 +11,7 @@ namespace _3семестр._2лаба_шарп_
         [STAThread]
         static void Main()
         {
+            Console.WriteLine("The program is starting");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
